c74498aa3594bd342531395a3e77a7133e9bda13
LELR Foxtrot <lelr_foxtrot@mda.space>
2025-10-25 16:26:21 -0400

    Change to accept a dynamic number of topics.
    Store share pointers to prevent memory leak

--- include/pointcloud_concatenate/pointcloud_concatenate.hpp
+++ include/pointcloud_concatenate/pointcloud_concatenate.hpp
@@ -24,57 +24,33 @@
 class PointcloudConcatenate : public rclcpp::Node
 {
 public:
+  using PointCloudMsg = sensor_msgs::msg::PointCloud2;
+
   // Constructor and destructor
   PointcloudConcatenate();
   ~PointcloudConcatenate();
 
   // Public functions
-  void handleParams();
   void update();
   double getHz();
 
-  // Public variables and objects
 
 private:
   // Parameters
   std::string param_frame_target_;
   int param_clouds_;
   double param_hz_;
-  std::string cloud_in1_topic_;
-  std::string cloud_in2_topic_;
-  std::string cloud_in3_topic_;
-  std::string cloud_in4_topic_;
-  std::string cloud_out_topic_;
 
   // Publisher and subscribers
-  rclcpp::Subscription<sensor_msgs::msg::PointCloud2>::SharedPtr sub_cloud_in1_;
-  rclcpp::Subscription<sensor_msgs::msg::PointCloud2>::SharedPtr sub_cloud_in2_;
-  rclcpp::Subscription<sensor_msgs::msg::PointCloud2>::SharedPtr sub_cloud_in3_;
-  rclcpp::Subscription<sensor_msgs::msg::PointCloud2>::SharedPtr sub_cloud_in4_;
+  std::vector<rclcpp::Subscription<PointCloudMsg>::SharedPtr> pc_subs_;
   rclcpp::Publisher<sensor_msgs::msg::PointCloud2>::SharedPtr pub_cloud_out_;
 
   // Private functions
-  void subCallbackCloudIn1(const sensor_msgs::msg::PointCloud2::SharedPtr msg);
-  void subCallbackCloudIn2(const sensor_msgs::msg::PointCloud2::SharedPtr msg);
-  void subCallbackCloudIn3(const sensor_msgs::msg::PointCloud2::SharedPtr msg);
-  void subCallbackCloudIn4(const sensor_msgs::msg::PointCloud2::SharedPtr msg);
+  void subCallbackCloud(const PointCloudMsg::SharedPtr msg, const int index);
   void publishPointcloud(sensor_msgs::msg::PointCloud2 &cloud);
 
   // Other
-
-  sensor_msgs::msg::PointCloud2 cloud_in1_;
-  sensor_msgs::msg::PointCloud2 cloud_in2_;
-  sensor_msgs::msg::PointCloud2 cloud_in3_;
-  sensor_msgs::msg::PointCloud2 cloud_in4_;
-  sensor_msgs::msg::PointCloud2 cloud_out_;
-  bool cloud_in1_received_ = false;
-  bool cloud_in2_received_ = false;
-  bool cloud_in3_received_ = false;
-  bool cloud_in4_received_ = false;
-  bool cloud_in1_received_recent_ = false;
-  bool cloud_in2_received_recent_ = false;
-  bool cloud_in3_received_recent_ = false;
-  bool cloud_in4_received_recent_ = false;
+  std::vector<PointCloudMsg::SharedPtr> clouds_;
 
   // Initialization tf2 listener
   std::shared_ptr<tf2_ros::Buffer> tf_buffer_;
--- src/pointcloud_concatenate.cpp
+++ src/pointcloud_concatenate.cpp
@@ -1,22 +1,31 @@
 #include "pointcloud_concatenate/pointcloud_concatenate.hpp"
+#include <algorithm>
 
 // Constructor
 PointcloudConcatenate::PointcloudConcatenate() : Node("pointcloud_concatenate")
 {
   // Initialise variables / parameters to class variables
-  handleParams();
+  param_frame_target_ = declare_parameter<std::string>("target_frame", "base_link");
+  param_clouds_ = declare_parameter<int>("clouds", 2);
+  param_hz_ = declare_parameter<double>("hz", 10.0);
+
+  clouds_.resize(param_clouds_);
+  
+  // Queues size of 1 to only keep the most recent message
+
+  for (unsigned i = 0; i < param_clouds_; ++i) {
+    const auto topic_i = declare_parameter<std::string>("cloud_in" + std::to_string(i+1) + "_topic", "cloud_in" + std::to_string(i+1));
+    const auto pc_cb_i = [i, this](const PointCloudMsg::SharedPtr msg) {this->subCallbackCloud(msg, i);};
+    pc_subs_.push_back(create_subscription<sensor_msgs::msg::PointCloud2>(topic_i, 1, pc_cb_i));
+  }
 
   // Initialization tf2 listener
   tf_buffer_ = std::make_shared<tf2_ros::Buffer>(this->get_clock());
   tf_listener_ = std::make_shared<tf2_ros::TransformListener>(*tf_buffer_);
 
   // Initialise publishers and subscribers
-  // Queues size of 1 to only keep the most recent message
-  sub_cloud_in1_ = this->create_subscription<sensor_msgs::msg::PointCloud2>(cloud_in1_topic_, 1, std::bind(&PointcloudConcatenate::subCallbackCloudIn1, this, std::placeholders::_1));
-  sub_cloud_in2_ = this->create_subscription<sensor_msgs::msg::PointCloud2>(cloud_in2_topic_, 1, std::bind(&PointcloudConcatenate::subCallbackCloudIn2, this, std::placeholders::_1));
-  sub_cloud_in3_ = this->create_subscription<sensor_msgs::msg::PointCloud2>(cloud_in3_topic_, 1, std::bind(&PointcloudConcatenate::subCallbackCloudIn3, this, std::placeholders::_1));
-  sub_cloud_in4_ = this->create_subscription<sensor_msgs::msg::PointCloud2>(cloud_in4_topic_, 1, std::bind(&PointcloudConcatenate::subCallbackCloudIn4, this, std::placeholders::_1));
-  pub_cloud_out_ = this->create_publisher<sensor_msgs::msg::PointCloud2>(cloud_out_topic_, 1);
+  pub_cloud_out_ = this->create_publisher<sensor_msgs::msg::PointCloud2>(declare_parameter<std::string>("cloud_out_topic", "cloud_out"), 1);
+
 }
 
 // Destructor
@@ -27,56 +36,11 @@ PointcloudConcatenate::~PointcloudConcatenate()
   // delete pointer_name;
 }
 
-void PointcloudConcatenate::subCallbackCloudIn1(const sensor_msgs::msg::PointCloud2::SharedPtr msg)
+void PointcloudConcatenate::subCallbackCloud(const PointCloudMsg::SharedPtr msg, const int index)
 {
-  cloud_in1_ = *msg;
-  cloud_in1_received_ = true;
-  cloud_in1_received_recent_ = true;
+  clouds_[index] = msg;
 }
 
-void PointcloudConcatenate::subCallbackCloudIn2(const sensor_msgs::msg::PointCloud2::SharedPtr msg)
-{
-  cloud_in2_ = *msg;
-  cloud_in2_received_ = true;
-  cloud_in2_received_recent_ = true;
-}
-
-void PointcloudConcatenate::subCallbackCloudIn3(const sensor_msgs::msg::PointCloud2::SharedPtr msg)
-{
-  cloud_in3_ = *msg;
-  cloud_in3_received_ = true;
-  cloud_in3_received_recent_ = true;
-}
-
-void PointcloudConcatenate::subCallbackCloudIn4(const sensor_msgs::msg::PointCloud2::SharedPtr msg)
-{
-  cloud_in4_ = *msg;
-  cloud_in4_received_ = true;
-  cloud_in4_received_recent_ = true;
-}
-
-void PointcloudConcatenate::handleParams()
-{
-  this->declare_parameter("target_frame", "base_link");
-  this->declare_parameter("clouds", 2);
-  this->declare_parameter("hz", 10.0);
-  this->declare_parameter("cloud_in1_topic", "/cloud_in1");
-  this->declare_parameter("cloud_in2_topic", "/cloud_in2");
-  this->declare_parameter("cloud_in3_topic", "/cloud_in3");
-  this->declare_parameter("cloud_in4_topic", "/cloud_in4");
-  this->declare_parameter("cloud_out_topic", "/cloud_out");
-
-  this->get_parameter("target_frame", param_frame_target_);
-  this->get_parameter("clouds", param_clouds_);
-  this->get_parameter("hz", param_hz_);
-  this->get_parameter("cloud_in1_topic", cloud_in1_topic_);
-  this->get_parameter("cloud_in2_topic", cloud_in2_topic_);
-  this->get_parameter("cloud_in3_topic", cloud_in3_topic_);
-  this->get_parameter("cloud_in4_topic", cloud_in4_topic_);
-  this->get_parameter("cloud_out_topic", cloud_out_topic_);
-
-  RCLCPP_INFO(this->get_logger(), "Parameters loaded.");
-}
 
 double PointcloudConcatenate::getHz()
 {
@@ -91,107 +55,40 @@ void PointcloudConcatenate::update()
     return;
   }
 
-  if ((!cloud_in1_received_) && (!cloud_in2_received_) && (!cloud_in3_received_) && (!cloud_in4_received_))
+  if (std::all_of(clouds_.begin(), clouds_.end(), [](const PointCloudMsg::SharedPtr pc ) {return pc == nullptr;}))
   {
     RCLCPP_WARN(this->get_logger(), "No pointclouds received yet. Sleeping 1s...");
     return;
   }
 
   // Initialise pointclouds
-  sensor_msgs::msg::PointCloud2 cloud_to_concat;
-  cloud_out_ = sensor_msgs::msg::PointCloud2(); // Clear the output pointcloud
+  sensor_msgs::msg::PointCloud2 cloud_out; // Clear the output pointcloud
 
-  // Track success of transforms
-  bool success = true;
+  unsigned i = 0;
+  for (const auto& pc_ptr : clouds_) {
+    sensor_msgs::msg::PointCloud2 cloud_to_concat;
 
-  // Concatenate the first pointcloud
-  if (param_clouds_ >= 1 && success && cloud_in1_received_)
-  {
-    // Warn if cloud was not received since last update
-    if (!cloud_in1_received_recent_)
-    {
-      RCLCPP_WARN(this->get_logger(), "Cloud 1 was not received since last update, reusing last received message...");
+    if (pc_ptr == nullptr) { 
+      RCLCPP_WARN(this->get_logger(), "No cloud received for index %d", i);
+      continue;
     }
-    cloud_in1_received_recent_ = false;
 
-    // Transform pointcloud to the target frame
-    // Here we just assign the pointcloud directly to the output to ensure the secondary
-    // data is inherited correctly.
-    success = pcl_ros::transformPointCloud(param_frame_target_, cloud_in1_, cloud_out_, *tf_buffer_);
+    // Track success of transforms
+    bool success = pcl_ros::transformPointCloud(param_frame_target_, *pc_ptr, cloud_to_concat, *tf_buffer_);
     if (!success)
     {
-      RCLCPP_WARN(this->get_logger(), "Transforming cloud 1 from %s to %s failed!", cloud_in1_.header.frame_id.c_str(), param_frame_target_.c_str());
+      RCLCPP_WARN(this->get_logger(), "Transforming cloud from %s to %s failed! Skipping.", pc_ptr->header.frame_id.c_str(), param_frame_target_.c_str());
+    } else {
+      pcl::concatenatePointCloud(cloud_out, cloud_to_concat, cloud_out);
     }
+    ++i;
   }
 
-  // Concatenate the second pointcloud
-  if (param_clouds_ >= 2 && success && cloud_in2_received_)
-  {
-    // Warn if cloud was not received since last update
-    if (!cloud_in2_received_recent_)
-    {
-      RCLCPP_WARN(this->get_logger(), "Cloud 2 was not received since last update, reusing last received message...");
-    }
-    cloud_in2_received_recent_ = false;
-
-    // Transform pointcloud to the target frame
-    success = pcl_ros::transformPointCloud(param_frame_target_, cloud_in2_, cloud_to_concat, *tf_buffer_);
-    if (!success)
-    {
-      RCLCPP_WARN(this->get_logger(), "Transforming cloud 2 from %s to %s failed!", cloud_in2_.header.frame_id.c_str(), param_frame_target_.c_str());
-    }
-
-    // Concatenate the pointcloud
-    if (success)
-    {
-      pcl::concatenatePointCloud(cloud_out_, cloud_to_concat, cloud_out_);
-    }
-  }
-
-  // Concatenate the third pointcloud
-  if (param_clouds_ >= 3 && success && cloud_in3_received_)
-  {
-    // Warn if cloud was not received since last update
-    if (!cloud_in3_received_recent_)
-    {
-      RCLCPP_WARN(this->get_logger(), "Cloud 3 was not received since last update, reusing last received message...");
-    }
-    cloud_in3_received_recent_ = false;
-
-    // Transform pointcloud to the target frame
-    // Here we just assign the pointcloud directly to the output to ensure the secondary
-    // data is inherited correctly.
-    success = pcl_ros::transformPointCloud(param_frame_target_, cloud_in3_, cloud_out_, *tf_buffer_);
-    if (!success)
-    {
-      RCLCPP_WARN(this->get_logger(), "Transforming cloud 3 from %s to %s failed!", cloud_in3_.header.frame_id.c_str(), param_frame_target_.c_str());
-    }
-  }
-
-  // Concatenate the third pointcloud
-  if (param_clouds_ >= 4 && success && cloud_in4_received_)
-  {
-    // Warn if cloud was not received since last update
-    if (!cloud_in4_received_recent_)
-    {
-      RCLCPP_WARN(this->get_logger(), "Cloud 4 was not received since last update, reusing last received message...");
-    }
-    cloud_in4_received_recent_ = false;
-
-    // Transform pointcloud to the target frame
-    // Here we just assign the pointcloud directly to the output to ensure the secondary
-    // data is inherited correctly.
-    success = pcl_ros::transformPointCloud(param_frame_target_, cloud_in4_, cloud_out_, *tf_buffer_);
-    if (!success)
-    {
-      RCLCPP_WARN(this->get_logger(), "Transforming cloud 4 from %s to %s failed!", cloud_in4_.header.frame_id.c_str(), param_frame_target_.c_str());
-    }
-  }
 
   // Publish the concatenated pointcloud
-  if (success)
+  if (cloud_out.height > 0)
   {
-    publishPointcloud(cloud_out_);
+    publishPointcloud(cloud_out);
   }
 }
 
